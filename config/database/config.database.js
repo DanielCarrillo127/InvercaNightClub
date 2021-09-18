d1eefa3f533e054d8b00dd8778e6689e93d3b2a9
danielcarrillo127 <decarrillo@uninorte.edu.co>
2021-09-18 10:48:54 -0500

    1st Version with config

---
+++ config/database/config.database.js
@@ -0,0 +1,36 @@
+const { Pool, Client } = require('pg')
+// const connectionString = process.env.DATABASE_URL
+const passwordDB = process.env.passwordDB
+
+
+function dbConnection() {
+
+    try {
+        const pool = new Pool({
+            database: 'InvercaDB',
+            user: 'postgres',
+            password: passwordDB,
+            port: 5432,
+            // ssl: {
+            //     rejectUnauthorized: false
+            // },
+            max: 20
+        })
+        console.log('Connected Database')
+        return pool;
+
+    } catch (error) {
+        console.log(error);
+        throw new Error('Database Connection Error')
+    }
+
+
+}
+
+module.exports = {
+    dbConnection
+}
+
+
+
+
